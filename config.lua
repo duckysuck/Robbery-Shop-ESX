a6457cbeb4d900e0d34b0d6029db13b8d833f25f
Abel Gaming <lcpdpolice4939@gmail.com>
2021-02-22 23:25:39 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Sell Location
+Config.Location = vector3(746.72, -1399.93, 26.58)
+
+-- Text Distance
+Config.Distance = 5
+
+-- Sell Prices
+Config.LaptopPrice = 250   --Laptop
+Config.PhonePrice = 120    --Phone
+Config.SaffronPrice = 50   --Saffron
+Config.SpeakerPrice = 75   --Speaker
+Config.CokePrice = 1000    --Coke
+Config.DiamondPrice = 750  --Diamond
+Config.ToothpastePrice = 200 --Toothpaste
